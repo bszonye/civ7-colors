3e69187762c0cfb699b28a98afa2cfdb9b686354
B. Szonye <bszonye@gmail.com>
2025-08-15 01:25:09 -0700

    add conflict analyzer

--- ui/bz-colors/bz-colors.js
+++ ui/bz-colors/bz-colors.js
@@ -36,14 +36,14 @@ document.body.classList.add("bz-colors");
 
 const colors = Database.query('gameplay', 'SELECT * FROM Colors');
 if (colors) {
-    const parseRGB = (rgbText) => {
+    function parseRGB(rgbText) {
         const [r, g, b, ..._] = rgbText.split(',');
         const rgb = [r, g, b];
         const color = (b << 16) | (g << 8) | r;
         const hex = '#' + ((r << 16) | (g << 8) | b).toString(16).padStart(6, '0');
         return { rgb, color, hex };
     }
-    const mapColors = (colors, prefix) => {
+    function mapColors(colors, prefix) {
         const c = new Map();
         for (const row of colors.filter(c => c.Type.startsWith(prefix))) {
             const key = row.Type.slice(prefix.length);
@@ -53,20 +53,44 @@ if (colors) {
         }
         return c;
     }
-    const showConflicts = (colors, threshold) => {
-        const keys = colors.keys();
+    function _showConflicts(cmap, threshold) {
+        const keys = [...cmap.keys()];
         for (let i = 0; i < keys.length - 1; ++i) {
             for (let j = i + 1; j < keys.length; ++j) {
                 const ki = keys[i];
                 const kj = keys[j];
-                const c1 = colors.get(ki).color;
-                const c2 = colors.get(kj).color;
-                const cd = Color.colorDistance(c1, c2);
-                if (cd < threshold) console.warn(`TRIX X ${ki} ${kj} ${cd}`);
+                const c1 = cmap.get(ki);
+                const c2 = cmap.get(kj);
+                const cd = Color.colorDistance(c1.color, c2.color);
+                if (cd < threshold) {
+                    console.warn(`TRIX X${threshold} ${ki} ${kj} ${cd.toFixed(3)}`);
+                }
+            }
+        }
+    }
+    function showClosest(cmap1, cmap2) {
+        for (const [ka, ca1] of cmap1.entries()) {
+            const cdlist = [];
+            for (const [kb, cb1] of cmap1.entries()) {
+                if (ka == kb) continue;
+                const cd1 = Color.colorDistance(ca1.color, cb1.color);
+                const ca2 = cmap2.get(ka);
+                const cb2 = cmap2.get(kb);
+                const cd2 = Color.colorDistance(ca2.color, cb2.color);
+                cdlist.push({ ka, kb, cd1, cd2, });
+            }
+            cdlist.sort((a, b) => a.cd1 - b.cd1);
+            for (const [i, cd] of cdlist.entries()) {
+                if (i == 0 || cd.cd1 < 4 || cd.cd1 + cd.cd2 < 10) {
+                    const cd1 = cd.cd1.toFixed(2).padStart(6, ' ');
+                    const cd2 = cd.cd2.toFixed(2).padStart(6, ' ');
+                    console.warn(`TRIX MIN ${cd1} ${cd2} ${cd.ka} ${cd.kb}`);
+                }
             }
         }
     }
     const primary = mapColors(colors, "BZ1_");
-    const _secondary = mapColors(colors, "BZ2_");
-    showConflicts(primary, 4);
+    const secondary = mapColors(colors, "BZ2_");
+    console.warn(`TRIX CLOSEST`);
+    showClosest(primary, secondary);
 }
