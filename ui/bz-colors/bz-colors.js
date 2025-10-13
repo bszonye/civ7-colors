fbbd9f860ab61a95ef859bc042f0893d3b7095fe
B. Szonye <bszonye@gmail.com>
2025-10-13 00:17:59 -0700

    fix color name filters

--- ui/bz-colors/bz-colors.js
+++ ui/bz-colors/bz-colors.js
@@ -89,7 +89,7 @@ if (colors) {
     // const standard = mapColors(colors, /^COLOR_STANDARD_/);
     // showClosest(standard);
     console.warn(`TRIX CLOSEST`);
-    const bzcolors = mapColors(colors, /^COLOR_BZ_[A-Z]+_?[A-Z]/, /^COLOR_BZ_/);
+    const bzcolors = mapColors(colors, /^COLOR_BZ_[A-Z]+(_LT|_MD|_DK)?$/, /^COLOR_BZ_/);
     showClosest(bzcolors);
     console.warn(`TRIX CLOSEST LT`);
     const ltcolors = mapColors(colors, /^COLOR_BZ_[A-Z]+_LT$/, /^COLOR_BZ_/);
