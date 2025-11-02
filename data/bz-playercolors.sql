ed06bb3c6fea8463ee61b8b40988501b6f4464a7
B. Szonye <bszonye@gmail.com>
2025-11-02 03:57:48 -0800

    Ibn Battuta: Morocco colors first

--- data/bz-playercolors.sql
+++ data/bz-playercolors.sql
@@ -285,9 +285,9 @@ WHERE Type="LEADER_HIMIKO";
 -- green: PMS 363C  #4c8c2b  oklch(0.5764 0.1459 136.84)
 UPDATE PlayerColors SET
 Alt3PrimaryColor="COLOR_BZ_GRAY_DK", Alt3SecondaryColor="COLOR_BZ_AMBER_LT",
-Alt2PrimaryColor="COLOR_BZ_RED_DK", Alt2SecondaryColor="COLOR_BZ_OLIVE_LT",
-Alt1PrimaryColor="COLOR_BZ_OLIVE_MD", Alt1SecondaryColor="COLOR_BZ_YELLOW_LT",
-PrimaryColor="COLOR_BZ_OLIVE_DK", SecondaryColor="COLOR_BZ_YELLOW_LT"
+Alt2PrimaryColor="COLOR_BZ_OLIVE_MD", Alt2SecondaryColor="COLOR_BZ_YELLOW_LT",
+Alt1PrimaryColor="COLOR_BZ_OLIVE_DK", Alt1SecondaryColor="COLOR_BZ_YELLOW_LT",
+PrimaryColor="COLOR_BZ_RED_DK", SecondaryColor="COLOR_BZ_OLIVE_LT"
 WHERE Type="LEADER_IBN_BATTUTA";
 
 -- ISABELLA: flag of Spain, coat of arms of Castile and Léon
