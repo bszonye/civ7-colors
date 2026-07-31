a047dedb12512a82274053c565e0b3dabce7d80d
B. Szonye <bszonye@gmail.com>
2026-07-30 18:05:26 -0700

    update Amina colors

--- data/bz-playercolors.sql
+++ data/bz-playercolors.sql
@@ -147,7 +147,7 @@ WHERE Type="LEADER_ALEXANDER";
 -- green: L55-60 150°
 UPDATE PlayerColors SET
 Alt3PrimaryColor="COLOR_BZ_RED_DK", Alt3SecondaryColor="COLOR_BZ_AQUA_LT",
-Alt2PrimaryColor="COLOR_BZ_AMBER_MD", Alt2SecondaryColor="COLOR_BZ_AQUA_DK",
+Alt2PrimaryColor="COLOR_BZ_WHITE_BG", Alt2SecondaryColor="COLOR_BZ_AQUA_DK",
 Alt1PrimaryColor="COLOR_BZ_AQUA_DK", Alt1SecondaryColor="COLOR_BZ_AMBER_MD",
 PrimaryColor="COLOR_BZ_GREEN_DK", SecondaryColor="COLOR_BZ_WHITE"
 WHERE Type="LEADER_AMINA";
