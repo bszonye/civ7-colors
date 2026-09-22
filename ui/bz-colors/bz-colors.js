0d453471d14cca14085992668793d70c216a1493
B. Szonye <bszonye@gmail.com>
2026-09-22 03:13:28 -0700

    limit color reports to debug mode

--- ui/bz-colors/bz-colors.js
+++ ui/bz-colors/bz-colors.js
@@ -1,6 +1,8 @@
 Controls.loadStyle("/bz-colors/ui/bz-colors/bz-colors.css");
 document.body.classList.add("bz-colors");
 
+const BZ_DEBUG = false;
+
 function srgbToHex(rgba) {
   const { r, g, b, a } = rgba;
   const hex = "#" + ((r << 16) | (g << 8) | b).toString(16).padStart(6, "0");
@@ -34,50 +36,57 @@ function getColorLc(bg, fg) {
   fg = getLuminance(Color.convertToSRGB(fg));
   return getYLc(bg, fg);
 }
-
-const colors = Database.query("gameplay", "SELECT * FROM Colors");
-if (colors) {
+function fmtLc(name, bg, fg) {
+  const lc = getColorLc(bg, fg).toFixed(2).padStart(7, " ");
+  bg = srgbToHex(Color.convertToSRGB(bg));
+  fg = srgbToHex(Color.convertToSRGB(fg));
+  name = Locale.compose(name);
+  return { lc, bg, fg, name };
+}
+function debugColors() {
+  const colors = Database.query("gameplay", "SELECT * FROM Colors");
+  if (colors) {
     function parseRGB(rgbText) {
-        const [r, g, b, a] = rgbText.split(",");
-        const rgba = { r: Number(r), g: Number(g), b: Number(b), a: Number(a ?? 255) };
-        const color = Color.convertToPackedSRGB(rgba);
-        const hex = srgbToHex(rgba);
-        return { rgba, color, hex };
+      const [r, g, b, a] = rgbText.split(",");
+      const rgba = { r: Number(r), g: Number(g), b: Number(b), a: Number(a ?? 255) };
+      const color = Color.convertToPackedSRGB(rgba);
+      const hex = srgbToHex(rgba);
+      return { rgba, color, hex };
     }
     function mapColors(colors, match, trim) {
-        const c = new Map();
-        for (const row of colors.filter(c => c.Type.match(match))) {
-            const key = row.Type.replace(trim, "");
-            const { rgba, color, hex } = parseRGB(row.Color);
-            const color3D = row.Color3D && parseRGB(row.Color3D);
-            c.set(key, { rgba, color, hex, color3D });
-            // console.warn(`TRIX ${key} Color=${hex} Color3D=${color3D?.hex}`);
-        }
-        return c;
+      const c = new Map();
+      for (const row of colors.filter(c => c.Type.match(match))) {
+        const key = row.Type.replace(trim, "");
+        const { rgba, color, hex } = parseRGB(row.Color);
+        const color3D = row.Color3D && parseRGB(row.Color3D);
+        c.set(key, { rgba, color, hex, color3D });
+        // console.warn(`TRIX ${key} Color=${hex} Color3D=${color3D?.hex}`);
+      }
+      return c;
     }
     function showClosest(cmap1, cmap2) {
-        for (const [ka, ca1] of cmap1.entries()) {
-            const cdlist = [];
-            for (const [kb, cb1] of cmap1.entries()) {
-                if (ka == kb) continue;
-                const cd1 = Color.colorDistance(ca1.color, cb1.color);
-                const ca2 = cmap2?.get(ka);
-                const cb2 = cmap2?.get(kb);
-                const cd2 = ca2 && cb2 ?
-                    Color.colorDistance(ca2.color, cb2.color) : void 0;
-                cdlist.push({ ka, kb, cd1, cd2, });
-            }
-            cdlist.sort((a, b) => a.cd1 - b.cd1);
-            for (const [i, cd] of cdlist.entries()) {
-                const cd1 = cd.cd1;
-                const cd2 = cd.cd2 ?? 10;
-                if (i == 0 || cd1 < 4 || cd1 + cd2 < 10) {
-                    const cd1 = cd.cd1.toFixed(2).padStart(6, " ");
-                    const cd2 = cd.cd2?.toFixed(2).padStart(7, " ") ?? "";
-                    console.warn(`TRIX MIN ${cd1}${cd2} ${cd.ka} ${cd.kb}`);
-                }
-            }
+      for (const [ka, ca1] of cmap1.entries()) {
+        const cdlist = [];
+        for (const [kb, cb1] of cmap1.entries()) {
+          if (ka == kb) continue;
+          const cd1 = Color.colorDistance(ca1.color, cb1.color);
+          const ca2 = cmap2?.get(ka);
+          const cb2 = cmap2?.get(kb);
+          const cd2 = ca2 && cb2 ?
+            Color.colorDistance(ca2.color, cb2.color) : void 0;
+          cdlist.push({ ka, kb, cd1, cd2, });
         }
+        cdlist.sort((a, b) => a.cd1 - b.cd1);
+        for (const [i, cd] of cdlist.entries()) {
+          const cd1 = cd.cd1;
+          const cd2 = cd.cd2 ?? 10;
+          if (i == 0 || cd1 < 4 || cd1 + cd2 < 10) {
+            const cd1 = cd.cd1.toFixed(2).padStart(6, " ");
+            const cd2 = cd.cd2?.toFixed(2).padStart(7, " ") ?? "";
+            console.warn(`TRIX MIN ${cd1}${cd2} ${cd.ka} ${cd.kb}`);
+          }
+        }
+      }
     }
     // const primary = mapColors(colors, /^BZ1_/);
     // const secondary = mapColors(colors, /^BZ2_/);
@@ -86,7 +95,7 @@ if (colors) {
     // showClosest(standard);
     console.warn(`TRIX CLOSEST`);
     const bzcolors = mapColors(
-        colors, /^COLOR_BZ_[A-Z]+(_BG|_LT|_MD|_DK)?$/, /^COLOR_BZ_/
+      colors, /^COLOR_BZ_[A-Z]+(_BG|_LT|_MD|_DK)?$/, /^COLOR_BZ_/
     );
     showClosest(bzcolors);
     console.warn(`TRIX CLOSEST LT`);
@@ -98,30 +107,37 @@ if (colors) {
     console.warn(`TRIX CLOSEST DK`);
     const dkcolors = mapColors(colors, /^COLOR_BZ_[A-Z]+_DK$/, /^COLOR_BZ_/);
     showClosest(dkcolors);
+  }
 }
-const leaderLc = [];
-function dumpLc(name, bg, fg) {
-    const lc = getColorLc(bg, fg).toFixed(2).padStart(7, " ");
-    bg = srgbToHex(Color.convertToSRGB(bg));
-    fg = srgbToHex(Color.convertToSRGB(fg));
-    name = Locale.compose(name);
-    leaderLc.push({ lc, bg, fg, name });
-}
-if (UI.isInShell()) {
+function debugLeaders() {
+  const leaderLc = [];
+  if (UI.isInShell()) {
     const leaders =
-        Database.query("config", "select LeaderType, LeaderName from Leaders");
+      Database.query("config", "select LeaderType, LeaderName from Leaders");
     for (const {LeaderType, LeaderName} of leaders) {
-        const hash = Database.makeHash(LeaderType);
-        const colors = UI.Color.getDefaultColorsAsHex(hash);
-        if (colors) dumpLc(LeaderName, colors.primaryColor, colors.secondaryColor);
+      const hash = Database.makeHash(LeaderType);
+      const colors = UI.Color.getDefaultColorsAsHex(hash);
+      if (colors) {
+        const row = fmtLc(LeaderName, colors.primaryColor, colors.secondaryColor);
+        leaderLc.push(row);
+      }
     }
-} else {
+  } else {
     for (const player of Players.getAlive()) {
-        const colors = UI.Color.getPlayerColors(player.id);
-        if (colors) dumpLc(player.leaderName, colors.primaryColor, colors.secondaryColor);
+      const colors = UI.Color.getPlayerColors(player.id);
+      if (colors) {
+        const row = fmtLc(player.leaderName, colors.primaryColor, colors.secondaryColor);
+        leaderLc.push(row);
+      }
     }
-}
-leaderLc.sort((a, b) => Math.abs(b.lc) - Math.abs(a.lc));
-for (const { lc, bg, fg, name } of leaderLc) {
+  }
+  leaderLc.sort((a, b) => Math.abs(b.lc) - Math.abs(a.lc));
+  for (const { lc, bg, fg, name } of leaderLc) {
     console.warn(`TRIX Lc ${lc} ${bg} ${fg} ${name}`);
+  }
 }
+
+if (BZ_DEBUG) debugColors();
+if (BZ_DEBUG) debugLeaders();
+
+// vim: sw=2
